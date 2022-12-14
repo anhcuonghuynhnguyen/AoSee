61cff6083ba1c272431e3284cc8850a0ca73e085
Huỳnh Nguyễn Anh Cường <69959006+anhcuonghuynhnguyen@users.noreply.github.com>
2022-12-14 14:11:26 +0700

    Add files via upload

---
+++ AoSEE/fHelp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AoSEE
+{
+    public partial class fHelp : Form
+    {
+        public fHelp()
+        {
+            InitializeComponent();
+            this.Icon = ResourcePic.LogoIcon1;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btnQues_MouseHover(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.BackgroundImage = ResourcePic.HoverStart;
+        }
+        private void btnQues_MouseLeave(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.BackgroundImage = ResourcePic.btnStart1;
+        }
+
+        private void btnQues1_Click(object sender, EventArgs e)
+        {
+            fTutorial fTutorial = new fTutorial();
+            fTutorial.ShowDialog();
+        }
+        private void btnQues2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("31211024087 - Lê Trần Khánh Phú\r\n31211024275 - Huỳnh Nguyễn Anh Cường\r\n31211027645 - Huỳnh Trịnh Tiến Khoa\r\n31211027688 - Nguyễn Ngọc Tường Vy", "About us", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void btnQues3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The results will be saved in: \r\n" + Global.path + "\r\n\nYou can open it in Setting !!!", "Help", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void btnQues4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You can use the following operators: +, -, *, /, ^\r\nAnd brackets ( )", "Help", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void btnQues5_Click(object sender, EventArgs e)
+        {
+            Process.Start(new ProcessStartInfo { FileName = @"https://drive.google.com/drive/folders/1HXvAqzpQNOo5FKeIjdDfEVWBDgLy2bqb?usp=sharing", UseShellExecute = true });
+        }
+        private void btnQues6_Click(object sender, EventArgs e)
+        {
+            Process.Start(new ProcessStartInfo { FileName = @"https://github.com/anhcuonghuynhnguyen/AoSee-1", UseShellExecute = true });
+        }
+
+    }
+}
---
+++ AoSEE/fTutorial.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace AoSEE
+{
+    partial class fTutorial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fTutorial));
+            this.picTutor = new System.Windows.Forms.PictureBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picTutor)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picTutor
+            // 
+            this.picTutor.BackgroundImage = global::AoSEE.ResourcePic.tutorial;
+            this.picTutor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picTutor.Location = new System.Drawing.Point(339, 12);
+            this.picTutor.Name = "picTutor";
+            this.picTutor.Size = new System.Drawing.Size(533, 369);
+            this.picTutor.TabIndex = 0;
+            this.picTutor.TabStop = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBack.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnBack.Location = new System.Drawing.Point(11, 10);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(85, 26);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(11, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(322, 342);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(3, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 21);
+            this.label2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(5);
+            this.label1.Size = new System.Drawing.Size(319, 277);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // fTutorial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(884, 393);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.picTutor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "fTutorial";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "How to use ?";
+            ((System.ComponentModel.ISupportInitialize)(this.picTutor)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox picTutor;
+        private Button btnBack;
+        private Panel panel1;
+        private Label label2;
+        private Label label1;
+    }
+}
